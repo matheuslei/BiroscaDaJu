53df154ce3087eb1d017721b56522977937fc938
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2023-09-25 07:27:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-appendFile.use(cors());
+app.use(cors(corsOptions)); // Use this after the variable declaration
